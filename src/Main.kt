7b6848b8b8462635b5c447479c3bd5472605dfb6
nstor6 <nestrclase@gmail.com>
2024-11-27 19:26:21 +0100

    arreglo de fallos

--- src/Main.kt
+++ src/Main.kt
@@ -1,9 +1,9 @@
 fun main(){
     var tablero = Array(3){arrayOfNulls<String>(3)}
 
-    tablero[0]= arrayOf("X", "O", "X")
-    tablero[1]= arrayOf("X", "X", "O")
-    tablero[2]= arrayOf("X", "X", "X")
+    tablero[0]= arrayOf("O", "O", "O")
+    tablero[1]= arrayOf("O", "X", "X")
+    tablero[2]= arrayOf("O", "X", "X")
 
     print(Juego(tablero).jugar())
 
