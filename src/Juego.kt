5884bbf44d293f2213707489e7db3d4425099165
nestorClase <nestrclase@gmail.com>
2024-11-27 19:28:10 +0100

    arreglo de errores

--- src/Juego.kt
+++ src/Juego.kt
@@ -9,7 +9,7 @@ class Juego(val tablero: Array<Array<String?>>) {
             for (simbolo in fila) {
                 if (simbolo.equals("X") || simbolo.equals("x")) {
                     cntX++
-                } else if (simbolo.equals("O") || simbolo.equals("0") simbolo.equals("o")) {
+                } else if (simbolo.equals("O") || simbolo.equals("0") || simbolo.equals("o")) {
                     cntO++
                 }
             }
@@ -18,19 +18,26 @@ class Juego(val tablero: Array<Array<String?>>) {
         //Busque por chat gpt como moverme en diagonal por la matriz
         if (tablero[0][0] == tablero[1][1] && tablero[1][1] == tablero[2][2]) {
             resultado = tablero[0][0]
-        }else if(tablero[0][2] == tablero[1][1] && tablero[1][1] == tablero[2][0]){
+        }
+        if(tablero[0][2] == tablero[1][1] && tablero[1][1] == tablero[2][0]){
             resultado = tablero[0][2]
-        }else if (tablero[0][0] == tablero[1][0] && tablero[1][0] == tablero[2][0]){
+        }
+        if (tablero[0][0] == tablero[1][0] && tablero[1][0] == tablero[2][0]){
             resultado = tablero[0][0]
-        }else if (tablero[0][1] == tablero[1][1] && tablero[1][1] == tablero[2][1]){
+        }
+        if (tablero[0][1] == tablero[1][1] && tablero[1][1] == tablero[2][1]){
             resultado = tablero[0][1]
-        }else if(tablero[0][2] == tablero[1][2] && tablero[1][2] == tablero[2][2]){
+        }
+        if(tablero[0][2] == tablero[1][2] && tablero[1][2] == tablero[2][2]){
             resultado = tablero[0][2]
-        }else if(tablero[0][0] == tablero[0][1] && tablero[0][1] == tablero[0][2]){
+        }
+        if(tablero[0][0] == tablero[0][1] && tablero[0][1] == tablero[0][2]){
             resultado = tablero[0][0]
-        }else if(tablero[1][0] == tablero[1][1] && tablero[1][1] == tablero[1][2]){
+        }
+        if(tablero[1][0] == tablero[1][1] && tablero[1][1] == tablero[1][2]){
             resultado = tablero[1][0]
-        }else if(tablero[2][0] == tablero[2][1] && tablero[2][1] == tablero[2][2]){
+        }
+        if(tablero[2][0] == tablero[2][1] && tablero[2][1] == tablero[2][2]){
             resultado = tablero[2][0]
         }
 
@@ -38,9 +45,16 @@ class Juego(val tablero: Array<Array<String?>>) {
             resultado = "EMPATE"
         }
         if(cntO>cntX+1 || cntX>cntO+1){
-            resultado = null
+            resultado= null
+        }else if(tablero.size != 3){
+            resultado= null
         }
 
+        for(fila in tablero){
+            if(fila.size != 3){
+                resultado= null
+            }
+        }
         return resultado
     }
 
